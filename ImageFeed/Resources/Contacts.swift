cfa787a9565baf29a40e2d34f9718328891b761a
Georgy <georgin2000155@gmail.com>
2023-06-28 18:00:01 +0300

    -Added contacts.swift

---
+++ ImageFeed/Resources/Contacts.swift
@@ -0,0 +1,13 @@
+//
+//  Contacts.swift
+//  ImageFeed
+//
+//  Created by Georgy on 28.06.2023.
+//
+
+import Foundation
+let AccessKey = "8S7LE27OGcya2-TGh15Qex2QSxqxnSBZngmFok0n3IU"
+let SecretKey = "_aImhVFqZM88bGa7_OViB8RTcjDh7ZMX9qPmPS16eK8"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
