af0eafe139c2284e0f9262e766f604f728f15961
Georgy <georgin2000155@gmail.com>
2023-07-02 16:30:27 +0300

    - WebView created - Implemented web page output - UIProgress view added

---
+++ ImageFeed/Presentation/Auth/AuthViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Georgy on 28.06.2023.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    //MARK: - Varibles
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
+//MARK: - WebViewViewControllerDelegate
+extension AuthViewController: WebViewViewControllerDelegate{
+    
+    
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        vc.dismiss(animated: true)
+    }
+    
+    
+}
+
+//MARK: - Segue
+extension AuthViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier{
+            let viewController = segue.destination as! WebViewViewController
+            viewController.delegate = self
+        } else{
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
--- ImageFeed/Resources/Contacts.swift
+++ ImageFeed/Resources/Contacts.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+let UnsplashAuthorizeURLString = "https://api.unsplash.com"
 let AccessKey = "8S7LE27OGcya2-TGh15Qex2QSxqxnSBZngmFok0n3IU"
 let SecretKey = "_aImhVFqZM88bGa7_OViB8RTcjDh7ZMX9qPmPS16eK8"
 let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
